c2326db3660987d8aa7f04a08060bb9fd934a3af
Anas Hussam <anashuasam4@gmail.com>
2025-03-18 01:27:05 +0300

    The special functions for search and update are built into the DataAccess layer.

--- DataAccess/obj/Debug/net9.0-windows/Database.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net9.0-windows/Database.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Database")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e42c496b922dc5521e575a9410bcd25c8885214")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f03a12ee67656b3c71685f1e0cfd5db2fe30d6fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Database")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Database")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
