ca2a7126c0009b55ffa3d330ce8800721e502c4d
Ahmed Atef <ahmedatef10987@gmail.com>
2025-03-18 10:18:26 +0200

    s

--- DataAccess/obj/Debug/net9.0-windows/Database.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net9.0-windows/Database.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Database")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e42c496b922dc5521e575a9410bcd25c8885214")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f03a12ee67656b3c71685f1e0cfd5db2fe30d6fa")]
->>>>>>> c2326db3660987d8aa7f04a08060bb9fd934a3af
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a9be8c1fe1f26593d9fe5dfe84dece5e814335e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Database")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Database")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
